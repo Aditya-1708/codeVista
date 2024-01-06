067264cb6bc1a0843fcf7f93853634f010a7d2a0
unknown <Aditya-1708>
2024-01-06 20:45:07 +0530

    changes

--- api/AIservice/askAI.js
+++ api/AIservice/askAI.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai"
-async function askAI(error){
-    const openai=new OpenAI({apiKey: "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"});
+async function askAI(APIKEY,error){
+    const openai=new OpenAI({apiKey: APIKEY});
     const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [{"role": "user", "content": error}],
